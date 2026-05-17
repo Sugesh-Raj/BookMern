2c6f2063582da481d3b309b192b679a2a3651169
sugesh <sugeshjr10@gmail.com>
2026-05-17 16:04:01 +0530

    Configured for deployment and added CI/CD pipeline

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 // ✅ middleware FIRST
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }))
 
